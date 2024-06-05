db0bdc10116c590508e4fc1520b57bd3fa49b0bb
Pablo Roberto <pablomoura2016@gmail.com>
2024-06-05 16:39:34 -0300

    adicionei o multiprovider caso tenha outros providers

--- taramps/lib/src/main.dart
+++ taramps/lib/src/main.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:taramps/src/pages/home_screen.dart';
+import 'package:taramps/src/theme/theme_provider.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   // BUSCA O ULTIMO TEMA SALVO PELO USUARIO
   SharedPreferences prefs = await SharedPreferences.getInstance();
   bool isDark = prefs.getBool('isDarkTheme') ?? false;
 
-
-
-  runApp(const MyApp());
+  ThemeProvider themeProvider = ThemeProvider(isDark);
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(value: themeProvider),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -21,19 +29,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(
-        colorScheme: const ColorScheme.dark(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark().copyWith(
+            colorScheme: const ColorScheme.dark(
           primary: Colors.white,
-        )
-      ),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const HomeScreen()
-      }
-    );
+        )),
+        initialRoute: '/',
+        routes: {'/': (context) => const HomeScreen()});
   }
 }
-
-
